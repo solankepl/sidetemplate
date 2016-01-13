1d2ecde5c6b3b7fdc8962cb00c459d25eb6dad01
Pavan Solanke <solankepl@rediff.com>
2016-01-13 05:41:52 +0000

    add wow effect

--- js/main.js
+++ js/main.js
@@ -6,6 +6,18 @@ $(function() {
 function initApp(){
     addEvent();
     $(".rslides").responsiveSlides();
+    
+      var wow = new WOW({
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       0,          // default
+                      mobile:       true,       // default
+                      live:         true        // default
+                    });
+      wow.init();
+    
+    
+    
 }
 
 function addEvent(){
